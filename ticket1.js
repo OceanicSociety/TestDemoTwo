02c645172e92a1893775a2e50e64375a8163d02a
Ravi <18ravibhatt@gmail.com>
2023-06-16 16:23:54 +1200

    ticket1 commit

---
+++ ticket1.js
@@ -0,0 +1,17 @@
+// This is a simple JavaScript file
+
+// Function to calculate the sum of two numbers
+function calculateSum(a, b) {
+  return a + b;
+}
+
+// Function to display a greeting
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// Example usage of the functions
+var result = calculateSum(5, 3);
+console.log("Sum:", result);
+
+greet("GitKraken");
